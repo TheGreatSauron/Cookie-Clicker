57fe3ce7489139e69559264eaf9e46090c8c4aec
newrecycle <33557350+newrecycle@users.noreply.github.com>
2017-11-10 15:14:44 -0700

    Window variable became window
    Easier to remember and more common practice

--- Main.cpp
+++ Main.cpp
@@ -3,35 +3,35 @@
 int main()
 {
 	//Creates game window
-	sf::RenderWindow Window(sf::VideoMode(800, 800), "Cookie Clicker");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Cookie Clicker");
 
 	//Creates a "shape" object in the variable shape and makes it green
 	sf::CircleShape shape(400.f);
     shape.setFillColor(sf::Color::Green);
 
 	//Application runs until window is closed
-	while (Window.isOpen())
+	while (window.isOpen())
 	{
 		//Polls all input events per frame
 		sf::Event Event;
-		while (Window.pollEvent(Event))
+		while (window.pollEvent(Event))
 		{
 			//Searches for event type and applicable actions
 			switch(Event.type)
 			{
 				//Closes window
 				case sf::Event::Closed:
-					Window.close();
+					window.close();
 					break;
 			}
 		}
 
 		//Reset the window
-		Window.clear();
+		window.clear();
 		//Draw the "shape" we created (the circle)
-		Window.draw(shape);
+		window.draw(shape);
 		//Update the display on the screen
-		Window.display();
+		window.display();
 	}
 
 	return 0;
