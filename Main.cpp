4f4efaa25d8be54b6d715ebf537ce3dd5c25764a
newrecycle <33557350+newrecycle@users.noreply.github.com>
2017-11-10 14:55:47 -0700

    SFML now integrated into cbp file
    Should now be able to open project and build on any computer that has code blocks properly installed. Also added a green circle that is to be displayed in the centre of the screen as a test.

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,11 @@ int main()
 {
 	//Creates game window
 	sf::RenderWindow Window(sf::VideoMode(800, 800), "Cookie Clicker");
-	
+
+	//Creates a "shape" object in the variable shape and makes it green
+	sf::CircleShape shape(400.f);
+    shape.setFillColor(sf::Color::Green);
+
 	//Application runs until window is closed
 	while (Window.isOpen())
 	{
@@ -12,7 +16,7 @@ int main()
 		sf::Event Event;
 		while (Window.pollEvent(Event))
 		{
-			//Searchhes for event type and applicable actions
+			//Searches for event type and applicable actions
 			switch(Event.type)
 			{
 				//Closes window
@@ -21,13 +25,16 @@ int main()
 					break;
 			}
 		}
-		
+
 		//Reset the window
 		Window.clear();
-		
+		//Draw the "shape" we created (the circle)
+		Window.draw(shape);
 		//Update the display on the screen
 		Window.display();
 	}
-	
+
 	return 0;
 }
+
+
